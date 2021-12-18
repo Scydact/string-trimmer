694680b85da0b801483b512a318014eaad25d071
Scydact <scydact@gmail.com>
2021-12-17 22:40:06 -0400

    E x p a n d e d

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { Box, Button, Container, CssBaseline, Grid, Link as MUILink, Typography } from '@mui/material';
-import { memo, createElement } from 'react';
+import { DarkMode, LightMode, SettingsBrightness } from '@mui/icons-material';
+import { Box, Button, Container, CssBaseline, Grid, Link as MUILink, ToggleButton, ToggleButtonGroup, Typography, useMediaQuery } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import React, { memo, createElement, useMemo, useState, useCallback } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate, Link, LinkProps, useResolvedPath, useMatch } from 'react-router-dom';
 
 import NewLineTrimmer from './Pages/new-line-trimmer';
@@ -12,33 +14,79 @@ const pages = [
   }
 ]
 
+type PaletteMode = 'light' | 'dark' | 'auto';
+
+function useDarkMode() {
+  const [mode, setMode] = useState<PaletteMode>('auto');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  
+  const toggleMode = useCallback((mode?: PaletteMode) => {
+    if (mode) setMode(mode);
+    else setMode(prev => {
+      switch (prev) {
+        case 'light':
+          return 'auto';
+        case 'auto':
+          return 'dark';
+        case 'dark':
+        default:
+          return 'light';
+      }
+    });
+  }, [])
+
+  const theme = useMemo(() => createTheme({
+    palette: {
+      mode: 'light' || (mode === 'auto') ? (prefersDarkMode ? 'dark' : 'light') : mode,
+    }
+  }), [prefersDarkMode, mode])
+
+  return { mode, setMode, toggleMode, theme };
+}
+
 function App() {
+  const { mode, setMode, toggleMode, theme } = useDarkMode();
   return (
-    <Box>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Nav />
-        <CssBaseline />
-        <Container>
-          <Routes>
-            <Route index element={<Index />} />
-            {pages.map(x => <Route key={x.path} path={x.path} element={createElement(x.elem)} />)}
-          </Routes>
-        </Container>
-      </BrowserRouter>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Nav mode={mode} setMode={(s) => setMode(s)} />
+          <Container>
+            <Routes>
+              <Route index element={<Container><h1>Text utils available:</h1><Index /></Container>} />
+              {pages.map(x => <Route key={x.path} path={x.path} element={createElement(x.elem)} />)}
+            </Routes>
+          </Container>
+        </BrowserRouter>
+      </Box>
+    </ThemeProvider>
 
   );
 }
 
-const Nav = memo(function Nav() {
-  return <Box sx={{ bgcolor: 'ButtonFace', px: 3 }}>
+type NavProps = {
+  mode: PaletteMode,
+  setMode: (s: PaletteMode) => any,
+}
+
+const Nav = memo(function Nav({ mode, setMode }: NavProps) {
+  return <Box sx={{ bgcolor: 'primary.dark', px: 3 }}>
     <Grid container py={1} alignItems='center'>
       <Grid item flexGrow='1'>
-        <Typography>Text utils</Typography>
+        <Typography component={Link} to='/' sx={{ color: 'text.primary' }}>Text utils</Typography>
       </Grid>
 
       <Grid item container sx={{ width: 'auto' }} gap={2}>
         <Index />
+      </Grid>
+
+      <Grid item>
+        <ToggleButtonGroup value={mode} onChange={(evt, m) => setMode(m)} >
+          <ToggleButton value='light'><LightMode /></ToggleButton>
+          <ToggleButton value='auto'><SettingsBrightness /></ToggleButton>
+          <ToggleButton value='dark'><DarkMode /></ToggleButton>
+        </ToggleButtonGroup>
       </Grid>
     </Grid>
   </Box>
@@ -60,13 +108,14 @@ function CustomLink({ children, to, ...props }: LinkProps) {
   const match = useMatch({ path: resolved.pathname, end: true })
 
   // @ts-ignore
-  return <MUILink
-    underline={match ? 'always' : 'hover'}
+  return <Button
+    // underline={match ? 'always' : 'hover'}
+    variant={match ?  'outlined' : 'contained' }
     component={Link}
     to={to}
     {...props}>
     {children}
-  </MUILink>
+  </Button>
 }
 
 export default App;
--- src/Pages/new-line-trimmer.tsx
+++ src/Pages/new-line-trimmer.tsx
@@ -1,48 +1,182 @@
-import { ContentCopy, ContentPaste } from "@mui/icons-material";
-import { Button, Grid } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Backspace, ContentCopy, ContentCut, ContentPaste, CopyAll, Delete, Redo, Undo, FilterNone, Filter2 } from "@mui/icons-material";
+import { Box, Button, ButtonGroup, ButtonProps, Container, Grid, IconButton, Stack, TextField, Tooltip, useMediaQuery } from "@mui/material";
+import { useDebounce } from "@react-hook/debounce";
+import React, { useCallback, useEffect, useState } from "react";
+import useUndo from "use-undo";
+
+const LOREM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+
+
+
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+
+Excepteur sint occaecat cupidatat non proident, 
+
+sunt in culpa qui officia deserunt mollit anim id est laborum.
+`
+
+type ThisAction = {
+  name: React.ReactNode,
+  description: React.ReactNode,
+  fn: Function,
+  props: ButtonProps,
+}
+
+const actions: ThisAction[] = [
+  {
+    name: 'Trim all',
+    description: 'Trim all spaces, leaving only single new lines.',
+    fn: function (t: string) { return t.replaceAll(/(\r?\n){2,}/g, '\n') },
+    props: {
+      startIcon: (<FilterNone />)
+    },
+  },
+  {
+    name: 'Trim doubles',
+    description: 'Trim double spaces, fixing formatting on Office text.',
+    fn: function (t: string) { return t.replaceAll(/(\r?\n){2}/g, '\n') },
+    props: {
+      startIcon: (<Filter2 />)
+    },
+  },
+  {
+    name: 'Traditional trim',
+    description: 'Simply trim each line.',
+    fn: function (t: string) { return t.split('\n').map(x => x.trim()).join('\n').trim() },
+    props: {
+      startIcon: (<ContentCut />)
+    },
+  },
+]
+
+const SepBeforeFactory = (s: string, n = 80) => ({
+  name: <>{s.repeat(3)}<br />txt</>,
+  description: `Adds ${n} ${s.repeat(3)} before the text`,
+  fn: function (t: string) { return s.repeat(n) + '\n' + t },
+  props: {},
+})
+const SepAfterFactory = (s: string, n = 80) => ({
+  name: <>txt<br />{s.repeat(3)}</>,
+  description: `Adds ${n} ${s.repeat(3)} after the text`,
+  fn: function (t: string) { return t + '\n' + s.repeat(n) },
+  props: {},
+})
+
+const separators: ThisAction[] = [
+  SepBeforeFactory('-'),
+  SepBeforeFactory('='),
+  SepAfterFactory('-'),
+  SepAfterFactory('=',)
+]
+
+function createActionBtn(text: string, set: (s: string) => void, actions: ThisAction[]) {
+  return actions.map((x, i) => <Tooltip key={i} title={x.description || ''}>
+    <Button
+      onClick={() => set(x.fn(text))}
+      disabled={(text === x.fn(text))}
+      {...x.props}>
+      {x.name}
+    </Button>
+  </Tooltip>
+  )
+}
 
 export default function NewLineTrimmer() {
-  
-  const [og, setOg] = useState('');
-  const [n, setN] = useState('');
+  const [text, setText] = useState(LOREM);
+  const [debouncedText, setDebouncedText, setDebouncedTextNow] = useDebounce(text, 400);
+
+  const [textState, { set, undo, redo, canUndo, canRedo }] = useUndo(text);
   const [copied, setCopied] = useState(false);
 
-  useEffect(() => {
-    setN(og.replaceAll(/(\r?\n){2,}/g, '\n'));
+  const isSmallScreen = useMediaQuery('(max-width: 440px)');
+
+  // On textarea change
+  const onChange = useCallback((evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(evt.target.value);
+    setDebouncedText(evt.target.value);
     setCopied(false);
-  }, [og])
+  }, [])
 
 
-  return <>
+  // Changes from user input
+  useEffect(() => {
+    if (debouncedText !== textState.present) {
+      set(debouncedText);
+    }
+  }, [debouncedText]);
+
+  // Changes using undo/redo history, or from custom action (using set())
+  useEffect(() => {
+    setText(textState.present);
+    setDebouncedTextNow(textState.present);
+    setCopied(false);
+  }, [textState.present]);
+
+  const Buttons = () => <Grid container gap={2}>
+    {/* Undo/Redo */}
+    <ButtonGroup variant='contained'>
+      <Tooltip title='Undo'><Button onClick={undo} disabled={!canUndo}><Undo /></Button></Tooltip>
+      <Tooltip title='Redo'><Button onClick={redo} disabled={!canRedo}><Redo /></Button></Tooltip>
+    </ButtonGroup>
+
+    {/* Delete/Paste/Copy/Cut */}
+    <ButtonGroup variant='contained'>
+
+      <Tooltip title='Delete'>
+        <Button
+          onClick={() => set('')}>
+          <Backspace />
+        </Button>
+      </Tooltip>
+
+      <Tooltip title='Paste'>
+        <Button
+          onClick={() => { navigator.clipboard.readText().then(t => set(t)); }}>
+          < ContentPaste />
+        </Button>
+      </Tooltip>
+
+      <Tooltip title='Copy'>
+        <Button
+          onClick={() => {
+            navigator.clipboard.writeText(text);
+            setCopied(true);
+          }}
+          color={copied ? 'success' : undefined}>
+          <ContentCopy />
+        </Button>
+      </Tooltip>
+
+    </ButtonGroup>
+
+    {/* Trim actions */}
+    <ButtonGroup variant='contained' orientation={isSmallScreen ? 'vertical' : 'horizontal'}>
+      {createActionBtn(text, set, actions)}
+    </ButtonGroup>
+
+    {/* Separator actions */}
+    <ButtonGroup variant='contained' sx={{ flexWrap: 'wrap' }}>
+      {createActionBtn(text, set, separators)}
+    </ButtonGroup>
+  </Grid>
+
+
+  return (<Container >
     <h1>New Line Trimmer™ 3000©</h1>
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <Button
-          startIcon={<ContentCopy />}
-          onClick={() => navigator.clipboard.readText().then(t => setOg(t))}
-          variant='contained'>
-          Paste
-        </Button>
-        <textarea
-          style={{ resize: 'vertical', width: '100%', minHeight: '20em' }}
-          value={og}
-          onChange={e => setOg(e.target.value)} />
-      </Grid>
-
-      <Grid item xs={6}>
-        <Button
-          startIcon={<ContentPaste />}
-          onClick={() => navigator.clipboard.writeText(n).then(() => setCopied(true))}
-          color={copied ? 'success' : undefined}
-          variant='contained'>
-          Copy
-        </Button>
-        <textarea
-          style={{ resize: 'vertical', width: '100%', minHeight: '20em' }}
-          value={n}
-          onChange={() => { }} />
-      </Grid>
-    </Grid>
-  </>
+    <Box>
+      <Buttons />
+    </Box>
+    <br />
+    <Box>
+      <TextField
+        multiline
+        label='Text to trim'
+        placeholder={LOREM}
+        style={{ width: '100%' }}
+        value={text}
+        onChange={onChange} />
+    </Box>
+  </Container>)
 }
